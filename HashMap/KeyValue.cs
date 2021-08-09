d4c03fc945837ba070b7ade0edaa082f75d671cc
Akash-Patil-12 <patilakash121212@gmail.com>
2021-08-09 18:13:01 +0530

    [Akash] Add : Implemented Find frequency of words in a sentence

---
+++ HashMap/KeyValue.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HashTableDemo
+{
+    public struct KeyValue<K, V>
+    {
+        public K Key { get; set; }
+        public V Value { get; set; }
+    }
+}
